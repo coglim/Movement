03b48fb51d81659a149f6e3fd461a0bfd4ecd9a6
coglim <94656716+coglim@users.noreply.github.com>
2021-12-07 17:34:57 -0800

    Add files via upload

---
+++ animationStateController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animationStateController : MonoBehaviour
+{
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        Debug.Log(animator); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeys("d", "s", "w", "a"))
+        {
+            animator.SetBool("isMoving", true);
+        }
+    }
+}
